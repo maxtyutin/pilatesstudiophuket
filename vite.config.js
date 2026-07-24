e53d0185b7ba7a213a324922dfb95127745a19b1
Максим Тютин <maxtyutin@MacBook-Air.local>
2026-07-24 19:24:17 +0300

    Configure standalone offline HTML bundle for direct file:// opening

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.js.org/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/pilatesstudiophuket/',
+  plugins: [react(), viteSingleFile()],
+  base: './',
   server: {
     port: 4000,
     strictPort: false,
