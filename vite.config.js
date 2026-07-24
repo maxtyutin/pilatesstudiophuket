57fd6713bda2597fb8123ec69cb2da268fb540f8
Максим Тютин <maxtyutin@MacBook-Air.local>
2026-07-24 18:52:14 +0300

    Set base to /pilatesstudiophuket/ and add 404.html for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.js.org/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/pilatesstudiophuket/',
   server: {
     port: 4000,
     strictPort: false,
